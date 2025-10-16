6b7f539f701ddabc48a450cc336ff5e825ea49c9
KitStarVP1 <kitstarvp+1@gmail.com>
2025-10-15 23:33:05 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+
     // Shopping Cart Functionality
-    const addToCartButtons = document.querySelectorAll('.product-card .btn-outline-dark');
+    const addToCartButtons = document.querySelectorAll('.btn-outline-custom');
     const cartCountBadge = document.getElementById('cart-count');
     let cartItemCount = 0;
 
